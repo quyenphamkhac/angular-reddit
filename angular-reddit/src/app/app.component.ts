c25ea0732a902462c4fb04cb714f82bd0ad9d2b7
quyenphamkhac <quyenphamkhac@users.noreply.github.com>
2018-02-05 14:29:24 +0700

    upload full source code

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 5', 'http://angular.io', 10),
+      new Article('Fullstack', 'http://fullstack.io', 3),
+      new Article('React JS', 'http://reactjs.org', 15)
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
     return false;
+  }
+
+  sortedArticle(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
